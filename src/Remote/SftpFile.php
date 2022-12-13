0d17469f6eacbd0ef199086c362076ba3bb98a46
fabiankohnen <fk@ambimax.de>
2022-12-13 13:26:21 +0100

    BREAKING CHANGE: replaced Filemode consts with Filemode Enum changed file existence validation in openStream, removed duplications in SftpFile openStream

--- src/Remote/SftpFile.php
+++ src/Remote/SftpFile.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ambimax\File\Remote;
 
 use Ambimax\File\File;
+use Ambimax\File\FileMode;
 use phpseclib3\Net\SFTP;
 use phpseclib3\Net\SFTP\Stream;
 
@@ -15,10 +16,7 @@ class SftpFile extends File
     protected string $password;
     protected SFTP $sftp;
 
-    /**
-     * @param File::MODE_* $mode
-     */
-    public function __construct(string $hostname, string $username, string $password, string $filePath, string $mode)
+    public function __construct(string $hostname, string $username, string $password, string $filePath, FileMode $mode)
     {
         $this->hostname = $hostname;
         $this->username = $username;
@@ -28,24 +26,20 @@ class SftpFile extends File
         parent::__construct($filePath, $mode);
     }
 
-    protected function openStream(string $filePath, string $mode): void
+    protected function openStream(string $filePath, FileMode $mode): void
     {
         Stream::register();
 
-        $tmpFileHandle = fopen(sprintf('sftp://%s:%s@%s:22/%s',
-            $this->username,
-            $this->password,
-            $this->hostname,
-            $filePath),
-            $mode);
-
-        if (false === $tmpFileHandle) {
-            throw new \RuntimeException(sprintf('Could not open file \'%s\'.', $filePath));
-        }
-
-        $this->fileHandle = $tmpFileHandle;
-        $this->filePath = $filePath;
-        $this->mode = $mode;
+        parent::openStream(
+            sprintf(
+                'sftp://%s:%s@%s:22/%s',
+                $this->username,
+                $this->password,
+                $this->hostname,
+                $filePath
+            ),
+            $mode
+        );
     }
 
     public function rename(string $newPath): bool
